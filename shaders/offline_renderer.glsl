e825aaa47e64af6e76ebde494d0fbb05086a040a
Zack Pudil <zack.pudil@gmail.com>
2021-05-17 00:30:10 -0500

    Fixed monte carlo path tracing

--- shaders/offline_renderer.glsl
+++ shaders/offline_renderer.glsl
@@ -99,13 +99,13 @@ vec3 cosWeightedRandomHemisphereDirection( const vec3 n, inout float seed ) {
     return normalize(rr);
 }
 
-vec3 modifyDirectionWithRoughness( const vec3 n, const float roughness, inout float seed, float metal ) {
+vec3 modifyDirectionWithRoughness( const vec3 n, const float roughness, inout float seed) {
   	vec2 r = hash2(seed);
     
 	vec3  uu = normalize(cross(n, abs(n.y) > .5 ? vec3(1.,0.,0.) : vec3(0.,1.,0.)));
 	vec3  vv = cross(uu, n);
 	
-    float a = roughness*roughness*roughness*roughness*roughness;
+    float a = roughness*roughness*roughness*roughness;
     
 	float rz = sqrt(abs((1.0-r.y) / clamp(1.+(a - 1.)*r.y,.00001,1.)));
 	float ra = sqrt(abs(1.-rz*rz));
@@ -123,100 +123,111 @@ vec2 randomInUnitDisk(inout float seed) {
 	return r*vec2(sin(phi),cos(phi));
 }
 
+float ggx(float nov, float r) {
+    float k = pow(r + 1.0, 2.0)/8.0;
+    return nov/(nov*(1.0 - k) + k);
+}
+
+vec3 diffuseCoverage(vec3 nor, vec3 rd, vec3 l, Material mat) {
+    vec3 v = normalize(-rd);
+    vec3 h = normalize(v + l);
+    float hov = max(dot(h, v), 0);
+    float nol = max(dot(nor, l), 0);
+
+    vec3 f0 = mix(vec3(0.04), mat.albedo, mat.metal);
+    vec3 F = f0 + (1.0 - f0)*pow(max(1 - hov, 0), 5);
+
+    return (1.0 - F)*(1.0 - mat.metal)*mat.albedo*nol/PI;
+
+}
+
+vec3 specularCoverage(vec3 nor, vec3 rd, vec3 l, Material mat) {
+    vec3 v = normalize(-rd);
+    vec3 h = normalize(v + l);
+
+    float hov = max(dot(h, v), 0);
+
+    vec3 f0 = mix(vec3(0.04), mat.albedo, mat.metal);
+    vec3 F = f0 + (1.0 - f0)*pow(max(1 - hov, 0), 5);
+
+    float a = mat.roughness*mat.roughness;
+    float a2 = a*a;
+    float noh = max(dot(nor, h), 0.0);
+    float noh2 = noh*noh;
+
+    float D = a2/(pow(noh2*(a2 - 1.0) + 1.0, 2.0)*PI);
+
+    float nov = max(dot(nor, v), 0);
+    float nol = max(dot(nor, l), 0);
+    
+    float G = ggx(nov, mat.roughness)*ggx(nol, mat.roughness);
+
+    float denom = 4.0*nov*nol;
+    return F*D*G*nol/max(denom, 0.001);
+}
+
 // =================== END LIGHT TRACING BRDF FUNCTIONS ========================
 #define PATH_LENGTH 5
 
 vec3 sdfs_pathtrace(vec3 ro, vec3 rd, inout float seed) {
+    vec3 sig = vec3(1);
     vec3 col = vec3(0);
     bool isBackground = true;
-
-    Material firstMat;
 
     for(int i = 0; i < PATH_LENGTH; i++) {
         int mid = 0;
         float dist = sdfs_trace(ro, rd, maxDistance, mid);
 
         if(dist < maxDistance) {
+            isBackground = false;
             vec3 pos = ro + rd*dist;
             vec3 nor = sdfs_getNormal(pos);
             Material mat = getMaterial(pos, nor, mid);
+            vec3 f0 = mix(vec3(0.04), mat.albedo, mat.metal);
 
-            if (isBackground) {
-                firstMat = mat;
-            } else {
-                firstMat.albedo *= mat.albedo;
-            }
+            for(int i = 0; i < numberOfLights; i++) {
+                if(i >= numberOfLights) break;
+                Light light = lights[i];
 
-            isBackground = false;
+                vec3 lightDirection = light.type == 0
+                    ? normalize(light.position)
+                    : normalize(light.position - pos);
 
-            vec3 lightColor;
-            if (hasEnvMap == 1) {
-                float F = FresnelSchlickRoughness(max(0.0, -dot(nor, rd)), 0.04, mat.roughness);
-                vec3 lightDirection = normalize(F > hash1(seed) - mat.metal
-                    ? modifyDirectionWithRoughness(reflect(rd, nor), mat.roughness, seed, mat.metal)
-                    : cosWeightedRandomHemisphereDirection(nor, seed));
+                float lightDist = light.type == 0
+                    ? maxDistance
+                    : length(light.position - pos);
 
-                if (dot(lightDirection, nor) <= 0.0) {
-                    lightDirection = cosWeightedRandomHemisphereDirection(nor, seed);
+                float F = FresnelSchlickRoughness(max(0.0, -dot(nor, lightDirection)), 0.04, mat.roughness);
+                if (F > hash1(seed) - mat.metal) {
+                    vec3 coverage = clamp(specularCoverage(nor, rd, lightDirection, mat), 0, 1);
+
+                    if (mat.metal > 0.5) {
+                        col += sig*coverage*light.color;
+                    } else {
+                        col += coverage*light.color;
+                    }
+                } else {
+                    vec3 coverage = diffuseCoverage(nor, rd, lightDirection, mat);
+                    float sha = step(INFINITY, sdfs_trace(pos+nor*0.005, lightDirection, lightDist));
+                    col += sig*coverage*light.color*sha;
                 }
-
-                float sha = step(INFINITY, sdfs_trace(pos+nor*0.005, lightDirection, maxDistance));
-
-                lightColor = clamp(
-                    sdfs_getDirectLighting(
-                        nor, lightDirection, rd, mat, sha,
-                        1.0 - exp(-envExp*textureLod(prefilter, lightDirection, 4.0*mat.roughness).rgb)
-                    ),
-                    0, 1);
-            } else {
-                lightColor = vec3(0);
-            }
-
-            for(int i = 0; i < 10; i++) {
-                if (i >= numberOfLights) break;
-
-                vec3 lightDirection = lights[i].type == 0
-                    ? normalize(lights[i].position)
-                    : normalize(lights[i].position - pos);
-
-                float travelDistance = lights[i].type == 0 ? maxDistance : distance(lights[i].position, pos);
-
-                float sha = step(INFINITY, sdfs_trace(pos+nor*0.005, lightDirection, travelDistance));
-
-                lightColor += clamp(sdfs_getDirectLighting(
-                    nor, lightDirection, rd, mat, sha, lights[i].color
-                ), 0, 1);
-            }
-
-            if (numberOfLights > 0) {
-                int averageNumber = numberOfLights;
-                if (hasEnvMap == 1) averageNumber += 1;
-                lightColor /= float(averageNumber);
             }
 
             ro = pos;
             float F = FresnelSchlickRoughness(max(0.0, -dot(nor, rd)), 0.04, mat.roughness);
-            // set up ray to bounce from for next calculation.
             if (F  > hash1(seed) - mat.metal) {
-                if ( mat.metal > 0.5) {
-                    col += firstMat.albedo*lightColor;
-                } else {
-                   col += lightColor;
+                if (mat.metal > 0.5) {
+                    sig *= max(mat.albedo, vec3(0.04));
                 }
-                rd = modifyDirectionWithRoughness(reflect(rd, nor), mat.roughness, seed, mat.metal);
-                if (dot(rd, nor) <= 0.0) {
-                    rd = cosWeightedRandomHemisphereDirection(nor, seed);
-                }
+                rd = modifyDirectionWithRoughness(reflect(rd, nor), mat.roughness, seed);
             } else {
-                col += firstMat.albedo*lightColor;
+                sig *= mat.albedo;
                 rd = cosWeightedRandomHemisphereDirection(nor, seed);
             }
         } else {
             if (hasEnvMap == 1) {
                 if (isBackground) return textureLod(prefilter, rd, 0).rgb;
-                col += firstMat.albedo*(1.0 - exp(-envExp*textureLod(prefilter, rd, 4.0*firstMat.roughness).rgb));
-            } else {
-                if (isBackground) return vec3(0);
+                col += sig*(1.0 - exp(-500*textureLod(prefilter, rd, 0).rgb));
             }
             return col;
         }
